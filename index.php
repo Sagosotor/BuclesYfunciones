c3b2d1a3ad5d45fae489a8b2db6d533ba4af42f9
Sagosotor <61335509+Sagosotor@users.noreply.github.com>
2020-02-28 20:56:03 -0500

    Practica de Bucles for, foreach para arreglos en indexado

--- index.php
+++ index.php
@@ -18,6 +18,55 @@ echo("<br>");
 $arreglo=array('usuario1'=>"Juliana",'usuario2'=>"Santiago",'usuario3'=>"Lorena",'usuario4'=>"Cesar");
 print_r($arreglo);
 echo("<br>");
+echo("<br>.................................");
+
+// tamaño de arreglo
+$tamano=count($arreglo);
+echo("<br>");
+print_r ("El tamaño de nuestro arreglo es:".$tamano);
+echo("<br>");
+echo("<br>.................................");
+
+
+// BUCLES EN PHP (for)
+echo("<br>");
+for($centinela=1; $centinela<=10; $centinela++) {
+    print("<br>El valor de centinela es:".$centinela."<br>");
+}
+
+// Bucle for para reocorrer arreglos
+echo("<br>.................................");
+$arreglo=array("Juliana","Santiago","Lorena","Cesar");
+for($centinela=0;$centinela<count($arreglo);$centinela++){
+    echo("<br>".$arreglo[$centinela]."<br>");
+
+}
+
+// Bucle foreach para recorrer arreglos
+echo("<br>.................................");
+$arreglo=array('usuario1'=>"Juliana",'usuario2'=>"Santiago",'usuario3'=>"Lorena",'usuario4'=>"Cesar");
+foreach($arreglo as $clave=>$valor){
+    echo "<br>"."La clave es: ".$clave."<br>";
+    echo "<br>".($valor."<br>");
+
+}
+
+// Bucle foreach para arreglo indexado 
+echo("<br>.................................");
+$arreglo=array("Juliana","Santiago","Lorena","Cesar");
+foreach($arreglo as $clave=>$valor){
+    echo "<br> El valor del cajon es: ".$clave;
+    echo "<br> El valor del cajon es: ".$valor;
+}
+
+
+
+
+
+
+
+
+
 
 
 ?>
